d6b1c1913463c26f99c59413d37b0fa812efaa9a
the-arcade-01 <aashisharcade@gmail.com>
2020-04-03 16:42:04 +0530

    folder creation function

--- script.py
+++ script.py
@@ -1,8 +1,22 @@
 import os
 
+current_path = os.getcwd()
 
 # renaming function
 # prev_name = 'folder/sample.txt'
 # new_name = 'sample.txt'
 
 # os.rename(prev_name,new_name)
+
+# items = os.listdir()
+# print(items)
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. '+ directory)
+
+createFolder('./folder/')
+
