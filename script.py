e0203880b1e96e33ab1ed9072012956bc0d4e820
the-arcade-01 <aashisharcade@gmail.com>
2020-04-03 17:17:15 +0530

    mapped ext with filename

--- script.py
+++ script.py
@@ -8,15 +8,29 @@ current_path = os.getcwd()
 
 # os.rename(prev_name,new_name)
 
-# items = os.listdir()
-# print(items)
+items = os.listdir()
+print(items)
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print('Error: Creating directory. '+ directory)
+# def createFolder(directory):
+#     try:
+#         if not os.path.exists(directory):
+#             os.makedirs(directory)
+#     except OSError:
+#         print('Error: Creating directory. '+ directory)
 
-createFolder('./folder/')
+# createFolder('./folder/')
+
+extension_dict = {}
+
+for item in items:
+    filename_list = item.split('.')
+    ext = filename_list[-1]
+    del filename_list[-1]
+    filename = ''.join(filename_list)
+    if ext in extension_dict:
+        extension_dict[ext].append(filename)
+    else:
+        extension_dict[ext] = [filename]
+
+print(extension_dict)
 
