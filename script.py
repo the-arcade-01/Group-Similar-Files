5d0fc27b10bc691a07eecb134052a44909076680
the-arcade-01 <aashisharcade@gmail.com>
2020-04-03 17:39:41 +0530

    done

--- script.py
+++ script.py
@@ -2,21 +2,14 @@ import os
 
 current_path = os.getcwd()
 
-# renaming function
-# prev_name = 'folder/sample.txt'
-# new_name = 'sample.txt'
-
-# os.rename(prev_name,new_name)
-
 items = os.listdir()
-print(items)
 
-# def createFolder(directory):
-#     try:
-#         if not os.path.exists(directory):
-#             os.makedirs(directory)
-#     except OSError:
-#         print('Error: Creating directory. '+ directory)
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. '+ directory)
 
 # createFolder('./folder/')
 
@@ -34,3 +27,12 @@ for item in items:
 
 print(extension_dict)
 
+for item in extension_dict:
+    folder_name = f'{item}-Folder'
+    files = extension_dict[item]
+    directory = f'./{folder_name}/'
+    createFolder(directory)
+    for name in files:
+        prev_name = f'{name}.{item}'
+        new_name = f'{folder_name}/{name}.{item}'
+        os.rename(prev_name,new_name)
